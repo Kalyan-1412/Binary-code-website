0c537840a68d3065a8fa6c6a9a70446418ae6a4b
Kalyan-1412 <114206362+Kalyan-1412@users.noreply.github.com>
2022-09-23 12:01:53 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const button = document.querySelector('.button')
+const numbers = document.querySelectorAll('.number')
+const input = document.querySelector('.input')
+
+button.addEventListener('click', ()=>{
+
+    number = input.value
+
+
+    if(number >= 8) {
+        number = number - 8
+        numbers[0].innerHTML = '1'
+    } 
+    else {
+        numbers[0].innerHTML = '0'
+    }
+    
+    if(number >=4) {
+        number = number - 4
+        numbers[1].innerHTML = '1'
+    }
+    else {
+        numbers[1].innerHTML = '0'
+    }
+    
+    if(number >=2) {
+        number = number - 2
+        numbers[2].innerHTML = '1'
+    }
+    else {
+        numbers[2].innerHTML = '0'
+    }
+    
+    if(number >=1) {
+        number = number - 1
+        numbers[3].innerHTML = '1'
+    }
+    else {
+        numbers[3].innerHTML = '0'
+    }
+
+    if(number >0) {
+        numbers[0].innerHTML = 'Aw'
+        numbers[1].innerHTML = 'hell nah'
+        numbers[2].innerHTML = 'get'
+        numbers[3].innerHTML = 'out'
+    }
+}
+)
+
